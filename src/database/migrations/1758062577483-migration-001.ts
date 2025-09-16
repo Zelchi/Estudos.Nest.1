54740ada811ab46a5f2250c8a1e44de2eab6be50
Zelchi <tininhakawaii@gmail.com>
2025-09-16 20:51:54 -0300

    Remove usuario_id column from ProdutoEntity and corresponding migration

---
+++ src/database/migrations/1758062577483-migration-001.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Migration0011758062577483 implements MigrationInterface {
+    name = 'Migration0011758062577483'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "produtos" DROP COLUMN "usuario_id"`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "produtos" ADD "usuario_id" character varying(100) NOT NULL`);
+    }
+
+}
